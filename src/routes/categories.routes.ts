4a1964947a489480146148e6c6ebf9c3cf8805d1
brunogoniadis <brunogoniadislima@gmail.com>
2024-04-09 20:29:10 -0300

    fix: process file fix

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,6 @@
 import express, { Request, Response } from "express";
 const router = express.Router();
 
-import dotenv from "dotenv";
-dotenv.config();
-
 router.get("/categories", (req: Request, res: Response) => {
   const apiKey = process.env.API_KEY;
 
