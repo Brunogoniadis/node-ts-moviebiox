b847172da14a64b10c55d9dcc7180f11947188f1
brunogoniadis <brunogoniadislima@gmail.com>
2024-04-10 01:31:34 -0300

    feat: Created route to get categories

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,14 @@ dotenv.config()
 
 import categoryRouter from './routes/category.routes'
 
+import categoriesRouter from './routes/categories.routes'
+
 const app = express()
 const port = 3000
 
 app.use('/api', categoryRouter)
+
+app.use('/api', categoriesRouter)
 
 app.listen(port, () => {
   console.log(`Server is up!!!`)
---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosResponse } from 'axios'
+import express, { Request, Response, Router } from 'express'
+
+interface Genre {
+  id: number
+  name: string
+}
+
+const router: Router = express.Router()
+
+router.get('/categories', async (req: Request, res: Response) => {
+  try {
+    const apiKey = process.env.API_KEY
+
+    if (!apiKey) {
+      throw new Error('API key not found')
+    }
+
+    const response: AxiosResponse<{ genres: Genre[] }> = await axios.get(
+      'https://api.themoviedb.org/3/genre/movie/list',
+      {
+        params: {
+          api_key: apiKey,
+        },
+      }
+    )
+
+    const categories: string[] = response.data.genres.map(
+      (genre: Genre) => genre.name
+    )
+
+    res.status(200).json(categories)
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message })
+    } else {
+      res.status(500).json({ error: 'There was an internal server error.' })
+    }
+  }
+})
+
+export default router
