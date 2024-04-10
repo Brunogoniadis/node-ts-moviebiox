cce72eaccd0dee1b7bf597c74eaac8a1d3635a7d
brunogoniadis <brunogoniadislima@gmail.com>
2024-04-10 18:28:19 -0300

    feat: Created route to get movie info

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,16 @@ import categoryRouter from './routes/category.routes'
 
 import categoriesRouter from './routes/categories.routes'
 
+import movieRouter from './routes/movie.routes'
+
 const app = express()
 const port = 3000
 
 app.use('/api', categoryRouter)
 
 app.use('/api', categoriesRouter)
+
+app.use('/api', movieRouter)
 
 app.listen(port, () => {
   console.log(`Server is up!!!`)
